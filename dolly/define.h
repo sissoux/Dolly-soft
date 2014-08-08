ee51b6d5091754397f8a6eae90d832b83e71c7ba
sissoux <alexis.damiens59@gmail.com>
2014-08-08 14:40:37 +0200

    init functions / LCD

--- dolly/define.h
+++ dolly/define.h
@@ -10,4 +10,23 @@
 #define ENDSWA 17
 #define ENDSWB 18
 
+#define Vin A0
+
+
+//// LCD conf ////
+#define LCD_RS 2
+#define LCD_EN 3
+#define LCD_D7 4
+#define LCD_D6 5
+#define LCD_D5 6
+#define LCD_D4 7
+#define LCD_BL 8
+
+
+
+//// EEPROM Address book ////
+#define STATE_ADR 0
+#define MODE_ADR 1
+#define DIR_ADR 2
+
 
--- dolly/dolly.ino
+++ dolly/dolly.ino
@@ -1,12 +1,37 @@
 #include "define.h"
-
+#include <EEPROM.h>
 
 #include <ClickEncoder.h>
 #include <TimerOne.h>
+#include <LiquidCrystal.h>
 
 ClickEncoder *encoder;
 int16_t last, value;
 
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+int LCDBrightness = 255;
+boolean LCDupdate = false;
+
+enum mode{
+  MSM,                                                //Move Shoot Move mode, for timelapse
+  VT,                                                 //Video Traveling mode
+  MAN                                                 //Manual Mode
+};
+
+enum state{
+  ON,                                                 //Movement ON
+  STBY                                                //No movement
+}
+
+enum direction{
+  AB,                                                 // From point A to B
+  BA                                                  // From point B to A
+}
+
+
+state State = MSM;
+mode Mode = STBY;
+direction Dir = AB;
 
 void timerIsr()
 {
@@ -15,29 +40,99 @@ void timerIsr()
 
 void setup()
 {
+  initIO();
   Serial.begin(115200);
-  encoder = new ClickEncoder(KNOBA, KNOBB, KNOBSW);
+  initKnob();
   
-  Timer1.initialize(1000);
-  Timer1.attachInterrupt(timerIsr);
+  lcd.clear();
+  lcd.print("Motorized dolly ");
+  lcd.setCursor(0,1);
+  lcd.print("07/08/2014 V1.00"); 
   
-  last = -1;
+  
 }
 
 void loop()
 {
-  value += encoder->getValue();
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+}
+
+void updateKnob()
+(
+  value += encoder->getValue();
   if (value != last) 
   {
     last = value;
     Serial.print("Encoder Value: ");
     Serial.println(value);
   }
-  
   ClickEncoder::Button b = encoder->getButton();
   if (b != ClickEncoder::Open)
   {
-    
+
   }
 }
+
+void recall()
+{
+  State = EEPROM.read(STATE_ADR);
+  Mode = EEPROM.read(MODE_ADR);
+  Dir = EEPROM.read(DIR_ADR);
+}
+
+void save()
+{
+  EEPROM.write(STATE_ADR,State);
+  EEPROM.write(MODE_ADR,Mode);
+  EEPROM.write(DIR_ADR,Dir);
+}
+
+void initIO()
+{
+  pinMode(STEP,OUTPUT);
+  pinMode(DIR,OUTPUT);
+  pinMode(LCD_BL,OUTPUT);
+
+  pinMode(BSW,INPUT_PULLUP);
+  pinMode(RSW,INPUT_PULLUP);
+  pinMode(KNOBA,INPUT_PULLUP);
+  pinMode(KNOBB,INPUT_PULLUP);
+  pinMode(KNOBSW,INPUT_PULLUP);
+
+  pinMode(ENDSWA,INPUT_PULLUP);
+  pinMode(ENDSWB,INPUT_PULLUP);
+
+  pinMode(Vin,INPUT);
+}
+
+void initKnob()
+{
+  encoder = new ClickEncoder(KNOBA, KNOBB, KNOBSW);
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(timerIsr);
+  last = -1;
+}
+
+void LCD_update()
+{
+    if (LCDupdate)
+    {
+      LCDupdate = false;
+      
+      
+    }
+  
+}
+
+
